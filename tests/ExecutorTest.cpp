8d21bdac9e9c6e2e8832a4d1525bd28012853291
vici1117 <15540382158@163.com>
2024-10-16 21:17:58 +0800

    feat:Add test cases for turn left command

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -76,4 +76,48 @@ namespace adas
         const Pose target({0,-1,'S'});
         ASSERT_EQ(target,executor->State());
     }
+
+    TEST(ExecutorTest,should_return_N_given_command_is_L_and_facing_is_E)
+    {
+        //given
+        std::unique_ptr<Executor> executor(Executor::newExecutor({0,0,'E'}));
+        //when
+        executor->Execute("L");
+        //then
+        const Pose target({0,0,'N'});
+        ASSERT_EQ(target,executor->State());
+    }
+
+    TEST(ExecutorTest,should_return_S_given_command_is_L_and_facing_is_W)
+    {
+        //given
+        std::unique_ptr<Executor> executor(Executor::newExecutor({0,0,'W'}));
+        //when
+        executor->Execute("L");
+        //then
+        const Pose target({0,0,'S'});
+        ASSERT_EQ(target,executor->State());
+    }
+
+    TEST(ExecutorTest,should_return_W_given_command_is_L_and_facing_is_N)
+    {
+        //given
+        std::unique_ptr<Executor> executor(Executor::newExecutor({0,0,'N'}));
+        //when
+        executor->Execute("L");
+        //then
+        const Pose target({0,0,'W'});
+        ASSERT_EQ(target,executor->State());
+    }
+
+    TEST(ExecutorTest,should_return_E_given_command_is_L_and_facing_is_S)
+    {
+        //given
+        std::unique_ptr<Executor> executor(Executor::newExecutor({0,0,'S'}));
+        //when
+        executor->Execute("L");
+        //then
+        const Pose target({0,0,'E'});
+        ASSERT_EQ(target,executor->State());
+    }
 }
